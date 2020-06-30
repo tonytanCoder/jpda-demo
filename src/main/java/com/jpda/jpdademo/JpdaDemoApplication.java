0858711a09304a9fb328e2356c70a23a0bab274e
tanzanlong <tanzanlong@yy.com>
2020-06-30 14:06:15 +0800

    jpad jdi demo

---
+++ src/main/java/com/jpda/jpdademo/JpdaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jpda.jpdademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpdaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpdaDemoApplication.class, args);
+	}
+
+}
